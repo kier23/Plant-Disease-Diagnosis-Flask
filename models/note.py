5791a20ebce007309133a07a7a84921533100a59
kier23 <126538958+kier23@users.noreply.github.com>
2025-08-08 23:29:49 +0800

    Delete models directory

--- models/note.py
+++
@@ -1,9 +0,0 @@
-from extensions import db
-from datetime import datetime
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    diagnosis_id = db.Column(db.Integer, nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
